5de793e639d67496ae5177589a6bbcb57b4319b3
Phạm Đức Thắng <42572293+thangpdzergitas@users.noreply.github.com>
2019-05-09 01:03:49 +0700

    .

---
+++ face_recognizer.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+import os
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# iniciate id counter
+id = 0
+
+# names related to ids: example ==> Thang: id=1,  etc
+names = ['None', 'Thang', 'Minh', 'Dung', 'Tu', 'Truong']
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("image", cv2.WINDOW_FREERATIO)
+cv2.moveWindow('image', 0, 0)
+cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1366)  # set video width
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)  # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
+
+while True:
+    recognized = False
+    ret, img = cam.read()
+
+    ##    img = cv2.flip(img, -1) # Flip vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        # Check if confidence is less them 100 ==> "0" is perfect match
+
+        if (confidence < 100):
+            id = names[id]
+            if (confidence > 70):
+                print(confidence)
+                recognized = True
+                break
+            confidence = "  {0}%".format(round(confidence))
+
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(confidence))
+
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', img)
+
+    if (recognized):
+        break
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
